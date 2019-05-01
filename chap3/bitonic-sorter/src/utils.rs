7394179dffdf3b6f2692bd76ea6336c884fa0f06
cipepser <respepic@gmail.com>
2019-05-01 13:57:18 +0900

    add tests with util functions

--- chap3/bitonic-sorter/src/utils.rs
+++ chap3/bitonic-sorter/src/utils.rs
@@ -5,11 +5,13 @@ use rand_pcg::Pcg64Mcg;
 pub fn new_u32_vec(n: usize) -> Vec<u32> {
     let mut rng = Pcg64Mcg::from_seed([0; 16]);
 
-    let mut v = Vec::with_capacity(v);
+    rng.sample_iter(&Standard).take(n).collect()
+}
 
-    for _ in 0..n {
-        v.push(rng.sample(&StandardE));
-    }
+pub fn is_sorted_ascending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] <= pair[1])
+}
 
-    v
+pub fn is_sorted_descending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] >= pair[1])
 }
