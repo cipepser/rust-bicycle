89fc3c59823f2c6468e93a39ee56ffbb82bd75e1
cipepser <respepic@gmail.com>
2019-05-02 18:43:10 +0900

    add examples for FnMut and FnOnce

--- chap7/closures/src/main.rs
+++ chap7/closures/src/main.rs
@@ -18,4 +18,24 @@ fn main() {
     apply_fn_mut(&mut lookup, 'o');
     apply_fn_once(lookup, 'y');
     assert_eq!(s1, "read-only");
+
+    let mut s2 = "append".to_string();
+    let mut modify = |ch| {
+        s2.push(ch);
+        true
+    };
+//    apply_fn(&modify, 'e'); // error[E0525]: expected a closure that implements the `Fn` trait, but this closure only implements `FnMut`
+    apply_fn_mut(&mut modify, 'e');
+    apply_fn_once(modify, 'd');
+    assert_eq!(s2, "appended");
+
+    let s3 = "be converted".to_string();
+    let mut consume = |ch| {
+        let bytes = s3.into_bytes();
+        bytes.contains(&(ch as u8))
+    };
+//    apply_fn(&consume, 'b'); // error[E0525]: expected a closure that implements the `Fn` trait, but this closure only implements `FnOnce`
+//    apply_fn_mut(&mut consume, 'c'); // error[E0525]: expected a closure that implements the `FnMut` trait, but this closure only implements `FnOnce`
+    apply_fn_once(consume, 'd');
+//    assert_eq!(s3, "error"); // error[E0382]: borrow of moved value: `s3`
 }
