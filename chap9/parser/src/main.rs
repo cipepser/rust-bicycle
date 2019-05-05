b6a03489d1a7377e03026e613348751adcb440e3
cipepser <respepic@gmail.com>
2019-05-06 00:25:05 +0900

    add Error formatter

--- chap9/parser/src/main.rs
+++ chap9/parser/src/main.rs
@@ -442,7 +442,9 @@ impl From<LexError> for Error {
 }
 
 impl From<ParseError> for Error {
-    fn from(e: ParseError) -> Self {}
+    fn from(e: ParseError) -> Self {
+        Error::Parser(e)
+    }
 }
 
 impl FromStr for Ast {
@@ -454,8 +456,91 @@ impl FromStr for Ast {
     }
 }
 
+use std::fmt;
+
+impl fmt::Display for TokenKind {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        use self::TokenKind::*;
+        match self {
+            Number(n) => n.fmt(f),
+            Plus => write!(f, "+"),
+            Minus => write!(f, "-"),
+            Asterisk => write!(f, "*"),
+            Slash => write!(f, "/"),
+            LParen => write!(f, "("),
+            RParen => write!(f, ")"),
+        }
+    }
+}
+
+impl fmt::Display for Loc {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}-{}", self.0, self.1)
+    }
+}
+
+impl fmt::Display for LexError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::LexErrorKind::*;
+        let loc = &self.loc;
+        match self.value {
+            InvalidChar(c) => write!(f, "{}: invalid char '{}'", loc, c),
+            Eof => write!(f, "End of file"),
+        }
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::ParseError::*;
+        match self {
+            UnexpectedToken(tok) => write!(f, "{}: {} is no expected", tok.loc, tok.value),
+            NotExpression(tok) => write!(f,
+                                         "{}: '{}' is not a start of expression",
+                                         tok.loc, tok.value,
+            ),
+            NotOperator(tok) => write!(f,
+                                       "{}: '{}' is not an operator",
+                                       tok.loc, tok.value,
+            ),
+            UnclosedOpenParen(tok) => write!(f,
+                                             "{}: '{}' is not closed",
+                                             tok.loc, tok.value,
+            ),
+            RedundantExpression(tok) => write!(f,
+                                               "{}: expression after '{}' is redunndant",
+                                               tok.loc, tok.value,
+            ),
+            Eof => write!(f, "End of file"),
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "parser error")
+    }
+}
+
+use std::error::Error as StdError;
+
+impl StdError for LexError {}
+
+impl StdError for ParseError {}
+
+impl StdError for Error {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        use self::Error::*;
+        match self {
+            Lexer(lex) => Some(lex),
+            Parser(parse) => Some(parse),
+        }
+    }
+}
+
 use std::io;
 use std::str::FromStr;
+use std::fmt::Formatter;
 
 fn prompt(s: &str) -> io::Result<()> {
     use std::io::{stdout, Write};
@@ -479,7 +564,13 @@ fn main() {
             let ast = match line.parse::<Ast>() {
                 Ok(ast) => ast,
                 Err(e) => {
-                    unimplemented!()
+                    eprintln!("{}", e);
+                    let mut source = e.source();
+                    while let Some(e) = source {
+                        eprintln!("caused by {}", e);
+                        source = e.source()
+                    }
+                    continue;
                 }
             };
             println!("{:?}", ast);
