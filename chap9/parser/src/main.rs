0fc5c3277f43af8ee2b4da856a3ccc5130a25f02
cipepser <respepic@gmail.com>
2019-05-06 01:06:45 +0900

    implement evaluator

--- chap9/parser/src/main.rs
+++ chap9/parser/src/main.rs
@@ -576,6 +576,87 @@ fn show_trace<E: StdError>(e: E) {
     }
 }
 
+struct Interpreter;
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum InterpreterErrorKind {
+    DivisionByZero,
+}
+
+type InterpreterError = Annot<InterpreterErrorKind>;
+
+impl Interpreter {
+    pub fn new() -> Self {
+        Interpreter
+    }
+
+    pub fn eval(&mut self, expr: &Ast) -> Result<i64, InterpreterError> {
+        use self::AstKind::*;
+        match expr.value {
+            AstKind::Num(n) => Ok(n as i64),
+            AstKind::UniOp { ref op, ref e } => {
+                let e = self.eval(e)?;
+                Ok(self.eval_uniop(op, e))
+            }
+            AstKind::BinOp { ref op, ref l, ref r } => {
+                let l = self.eval(l)?;
+                let r = self.eval(r)?;
+                self.eval_binop(op, l, r)
+                    .map_err(|e| InterpreterError::new(e, expr.loc.clone()))
+            }
+        }
+    }
+
+    fn eval_uniop(&mut self, op: &UniOp, n: i64) -> i64 {
+        use self::UniOpKind::*;
+        match op.value {
+            UniOpKind::Plus => n,
+            UniOpKind::Minus => -n,
+        }
+    }
+
+    fn eval_binop(&mut self, op: &BinOp, l: i64, r: i64) -> Result<i64, InterpreterErrorKind> {
+        use self::BinOpKind::*;
+        match op.value {
+            BinOpKind::Add => Ok(l + r),
+            BinOpKind::Sub => Ok(l - r),
+            BinOpKind::Mult => Ok(l * r),
+            BinOpKind::Div => {
+                if r == 0 {
+                    Err(InterpreterErrorKind::DivisionByZero)
+                } else {
+                    Ok(l / r)
+                }
+            }
+        }
+    }
+}
+
+impl fmt::Display for InterpreterError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::InterpreterErrorKind::*;
+        match self.value {
+            InterpreterErrorKind::DivisionByZero => write!(f, "division by zero"),
+        }
+    }
+}
+
+impl StdError for InterpreterError {
+    fn description(&self) -> &str {
+        use self::InterpreterErrorKind::*;
+        match self.value {
+            InterpreterErrorKind::DivisionByZero => "the right hand expression of the division evaluates to zero",
+        }
+    }
+}
+
+impl InterpreterError {
+    fn show_diagnostic(&self, input: &str) {
+        eprintln!("{}", self);
+        print_annot(input, self.loc.clone());
+    }
+}
+
 use std::io;
 use std::str::FromStr;
 use std::fmt::Formatter;
