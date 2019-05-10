d0811c51ef2ad3275360d60f32f003f330c1c80c
cipepser <respepic@gmail.com>
2019-05-10 23:04:25 +0900

    divide count to lib file

--- chap10/wordcount/src/main.rs
+++ chap10/wordcount/src/main.rs
@@ -1,22 +1,8 @@
 use std::env;
-use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
-use regex::Regex;
+use std::io::BufReader;
 use std::fs::File;
 
-pub fn count(input: impl BufRead) -> HashMap<String, usize> {
-    let re = Regex::new(r"\w+").unwrap();
-    let mut freqs = HashMap::new();
-
-    for line in input.lines() {
-        let line = line.unwrap();
-        for m in re.find_iter(&line) {
-            let word = m.as_str().to_string();
-            *freqs.entry(word).or_insert(0) += 1;
-        }
-    }
-    freqs
-}
+use wordcount::count;
 
 fn main() {
     let filename = env::args().nth(1).expect("1 argument FILENAME required");
