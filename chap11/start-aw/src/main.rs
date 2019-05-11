f6855f2714ac414d05c110d7cf6f0b391917de37
cipepser <respepic@gmail.com>
2019-05-11 13:38:17 +0900

    implement hello_name

--- chap11/start-aw/src/main.rs
+++ chap11/start-aw/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{server, HttpRequest, Responder, App};
+use actix_web::{server, HttpRequest, Responder, App, Path};
 
 use serde_derive::*;
 
@@ -12,11 +12,15 @@ fn hello(req: &HttpRequest) -> impl Responder {
     format!("Hello {}!", to)
 }
 
+fn hello_name(to: Path<HelloPath>) -> impl Responder {
+    format!("Hello {}!", &to.name)
+}
+
 fn main() {
     server::new(|| {
         App::new()
             .resource("/", |r| r.f(hello))
-            .resource("/{name}", |r| r.f(hello))
+            .resource("/{name}", |r| r.with(hello_name))
     }).bind("localhost:3000")
         .expect("Can not bind to port 3000")
         .run();
