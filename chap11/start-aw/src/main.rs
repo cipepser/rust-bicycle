0da13fcf5efaa0da5e4064500b6bc710f1e14288
cipepser <respepic@gmail.com>
2019-05-11 13:35:02 +0900

    add start-aw

---
+++ chap11/start-aw/src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{server, HttpRequest, Responder, App};
+
+use serde_derive::*;
+
+#[derive(Deserialize)]
+struct HelloPath {
+    name: String,
+}
+
+fn hello(req: &HttpRequest) -> impl Responder {
+    let to = req.match_info().get("name").unwrap_or("world");
+    format!("Hello {}!", to)
+}
+
+fn main() {
+    server::new(|| {
+        App::new()
+            .resource("/", |r| r.f(hello))
+            .resource("/{name}", |r| r.f(hello))
+    }).bind("localhost:3000")
+        .expect("Can not bind to port 3000")
+        .run();
+}
